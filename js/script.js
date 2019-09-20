85a1a535eaf7fdeb8ca3086e27db7fef0ef70c44
aerovir <aeroviradmin@gmail.com>
2019-09-20 23:49:44 +0200

    gallary

--- js/script.js
+++ js/script.js
@@ -22,33 +22,4 @@ jQuery(document).ready(function() {
     $("#go-top").click(function() {
         window.location.href = "#"
     });
-
-    //slider
-
-    var slideNow = 1;
-    var slideCount = $('#slidewrapper').children().length;
-    var slideInterval = 3000;
-    var translateWidth = 0;
-    var switchInterval = setInterval(nextSlide, slideInterval);
-
-    $('[id=viewport]').hover(function() {
-        clearInterval(switchInterval);
-        }, function() {
-        switchInterval = setInterval(nextSlide, slideInterval);
-    });
-
-    function nextSlide() {
-        if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
-            $('[id=slidewrapper]').css('transform', 'translate(0, 0)');
-            slideNow = 1;
-        } else {
-            translateWidth = -$('[id=viewport]').width() * (slideNow);
-            $('[id=slidewrapper]').css({
-                'transform': 'translate(' + translateWidth + 'px, 0)',
-                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-                '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-            });
-            slideNow++;
-        }
-    }
 });

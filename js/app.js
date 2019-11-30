7839d575b28e909c74b7ecb10564504d3625480d
Lookrecius <bragin.boris@ya.ru>
2019-11-30 18:37:12 +0300

    added app.js -> script to filter photos by year

---
+++ js/app.js
@@ -0,0 +1,33 @@
+function buttonFilter() {
+    "use strict";
+    const buttons = document.querySelector('#buttons').children;
+    const items = document.querySelector('.row').children;
+    console.log(buttons);
+    console.log(items);
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function () {
+            console.log(this.getAttribute('data-target'));
+            for (let j = 0; j < buttons.length; j++) {
+                buttons[j].classList.remove('active')
+
+            }
+
+            this.classList.add('active')
+            let target = this.getAttribute('data-target');
+            for (let t = 0; t < items.length; t++) {
+                items[t].style.display = "none";
+                if (items[t].getAttribute('data-id') == target) {
+                    items[t].style.display = "block";
+                }
+                if (target == 'all') {
+                    items[t].style.display = "block";
+                }
+
+
+            }
+        })
+
+    }
+}
+buttonFilter();
